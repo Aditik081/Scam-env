6a9a43f0b5111b5efdf42268804e6ff0aa3bf97a
Aditik081 <aditik605@gmail.com>
2026-04-11 23:54:04 +0530

    modify range in inference

--- grader.py
+++ grader.py
@@ -2,12 +2,12 @@ class ScamGrader:
 
     def easy(self, action, observation, info):
         actual = info.get("actual")
-        return 0.85 if action == actual else 0.15
+        return 0.9 if action == actual else 0.1
 
     def medium(self, action, observation, info):
         actual = info.get("actual")
-        return 0.80 if action == actual else 0.45
+        return 0.85 if action == actual else 0.15
 
     def hard(self, action, observation, info):
         actual = info.get("actual")
-        return 0.75 if action == actual else 0.25
+        return 0.8 if action == actual else 0.2
